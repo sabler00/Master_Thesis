48f52b7ed36f144dc5b6b2dfb3736470301d8826
sabler00 <lerchsab00@gmail.com>
2024-01-18 12:11:15 +0100

    changes

--- plot_overlay.py
+++ plot_overlay.py
@@ -1,8 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-data1 = np.loadtxt('processed_file_H-H_rdf_CHOLi.dat')
-data2 = np.loadtxt('soper_2007_H-H.dat')
+data1 = np.loadtxt('processed_file_H-H_rdf.dat')
+data2 = np.loadtxt('soper_2007_H-H_shift.dat')
 
 plt.figure(figsize=(8, 6))
 
@@ -12,7 +12,7 @@ plt.plot(data2[:, 0], data2[:, 1], label='Soper 2007 H-H RDF', color='red', line
 
 plt.xlabel('r / Å', fontsize = 14)
 plt.ylabel('g(r) / -', fontsize = 14)
-plt.title('Comparison of Data from Experiment with Simulation (CHOLi.ff, 300K, 1 atm)', fontsize = 18)
+plt.title('Comparison of Data from Experiment with Simulation (ffield.reax, 300K, 1 atm)', fontsize = 18)
 plt.legend()
 
 plt.grid(True)
