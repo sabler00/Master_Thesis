34e83693f2419e6a43a7c60ab675abac47f8f0c8
Sabine Lerch <sal@ea1-c724.uibk.ac.at>
2024-06-27 13:02:16 +0200

    switch to df

--- compute_distance_substrates.py
+++ compute_distance_substrates.py
@@ -1,55 +1,57 @@
 import numpy as np
+import os
+import pandas as pd
 from statistics import mean
 
-def read_xyz(file_path):
-    with open(file_path, 'r') as file:
-        lines = file.readlines()
-    return lines
-
-def extract_z_values(lines, atom_indices):
-    z_values = []
+def extract_and_average_last_column(lines, atom_indices):
+    averages = []
     step_size = 1480
     header_lines = 3
-    num_steps = len(lines) // 1483
+    num_steps = len(lines) // (step_size + header_lines)
 
     for step in range(num_steps):
-            start = step * (step_size + header_lines)
-            end = start + step_size + header_lines
-            step_lines = lines[start:end]
-            
-            for index in atom_indices:
-                if len(step_lines[index].split()) >= 4:
-                    atom_data = step_lines[index].split()
-                    try:
-                        z_value = float(atom_data[2])
-                        z_values.append(z_value)
-                    except (IndexError, ValueError):
-                        print(f"Error extracting z-value from line {index}: Invalid format or missing data.")
-                else:
-                    print(f"Warning: Line {index} does not have enough columns.")
+        start = step * (step_size + header_lines)
+        end = start + step_size + header_lines
+        step_lines = lines[start:end]
+        
+        step_values = []
+        for index in atom_indices:
+            line_index = index + header_lines
 
-    return z_values
-  
-def calculate_average_z(file_path, indices_surface, indices_substrate):
-    lines = read_xyz(file_path)
-    atom_indices = indices_surface + indices_substrate
-    z_values = extract_z_values(lines, atom_indices)
-    
-    avg_z_surface = []
-    avg_z_substrate = []
+            columns = step_lines[line_index].split()[1:]
+            if len(columns) == 3: 
+                    last_column_value = float(columns[-1])
+                    step_values.append(last_column_value)
+                
+        average_value = sum(step_values) / len(step_values)
+        averages.append(average_value)
 
-    avg_z_surface.append(mean(z_values[:288] ) )
-    avg_z_substrate.append(mean(z_values[289:] ) )
-    
-    return avg_z_surface, avg_z_substrate
 
-indices_surface = list(range(1153, 1440))
-indices_substrate = list(range(1441, 1480)) 
+    return averages
 
-# File path to the .xyz file
+
+""" for step in range(num_steps):
+    step_frame = df[:,step*] """
+
+
 file_path = '/home/sal/qmcf_examples/graphite_qmmm_setup/graphite-md-02.xyz'
 
-avg_z_surface, avg_z_substrate = calculate_average_z(file_path, indices_surface, indices_substrate)
 
-print(f"Average z value for atoms 1152 to 1440: {avg_z_surface}")
-print(f"Average z value for atoms 1441 to 1480: {avg_z_substrate}")
+df = pd.read_csv(file_path, delim_whitespace=True, header = None, skiprows= 1)
+
+df = df.drop(df[df.iloc[:, 0] == 'X'].index)
+df = df.drop(df[df.iloc[:, 0] == '1481'].index)
+#df = df.drop(to_drop)
+#print(df[df.iloc[:,0] == 1481])
+#print(df[df.iloc[:, 0] == '1481'])
+
+
+df = df.reset_index(drop = True)
+#df = df.drop()
+print(df)
+
+
+""" atom_indices = range(1153, 1481)
+averages = extract_and_average_last_column(lines, atom_indices)
+print(averages)
+ """
