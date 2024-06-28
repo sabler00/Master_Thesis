339a5eee3f3a6669958c430f1d306615668381f5
Sabine Lerch <sal@ea1-c724.uibk.ac.at>
2024-06-28 12:14:19 +0200

    added tweaks

--- compute_distance_substrates.py
+++ compute_distance_substrates.py
@@ -1,7 +1,5 @@
-import numpy as np
-import os
 import pandas as pd
-from statistics import mean
+
 
 file_path = '/home/sal/qmcf_examples/graphite_qmmm_setup/graphite-md-02.xyz'
 
@@ -12,12 +10,16 @@ df = df.drop(df[df.iloc[:, 0] == 'X'].index) #drops dummy atom
 df = df.drop(df[df.iloc[:, 0] == '1481'].index) #drops atom count line at every new step
 
 df = df.reset_index(drop = True)
-print(df)
+#print(df)
 
 def process_step(step): 
     step_surface = step.iloc[1152:1439, 3] #defining subchunk for surface and substrate, 3 for z axis
-    step_substrate = step.iloc[1440:1479, 3]
-    return step_surface.mean(), step_substrate.mean() 
+    step_substrate= step.iloc[1440:1479]
+    step_substrate_carbon = step_substrate[step_substrate.iloc[:, 0] == 'C']
+    step_substrate_nitrogen = step_substrate[step_substrate.iloc[:, 0] == 'N']
+    step_substrate_oxygen = step_substrate[step_substrate.iloc[:, 0] == 'O']
+    step_substrate_hydrogen = step_substrate[step_substrate.iloc[:, 0] == 'H']
+    return step_surface.mean(), step_substrate.iloc[:, 3].mean(), step_substrate_carbon.iloc[:, 3].mean(), step_substrate_nitrogen.iloc[:, 3].mean(), step_substrate_oxygen.iloc[:, 3].mean(), step_substrate_hydrogen.iloc[:, 3].mean()
 
 step_size = 1480 # needs to be adjusted for atom count 
 
